f56068f00e93a7b8085886f9a01413e7ea6f74f1
Ronnie Lao <Ronnie_Lao@glic.com>
2018-09-21 23:08:38 -0400

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,22 +26,29 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay [1]){
-        console.log("You found a match!");
+        alert("You found a match!");
     }
     else {
-        console.log("Sorry try again");
+        alert("Sorry try again");
     }
 }
 
-var flipCard = function (cardId) {
-    console.log ("User flipped " + cards[cardId].rank);
-    console.log (cards[cardId].cardImage);
-    console.log (cards[cardId].suit);
+var flipCard = function () {
+    var cardId = this.getAttribute('data-id');
+    this.setAttribute('src',cards[cardId].cardImage);
     cardsInPlay.push(cards[cardId].rank);
     if (cardsInPlay.length === 2){
         checkForMatch();
     }    
 }
+var createBoard = function() {
+    for (var i = 0; i < cards.length ; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src','images/back.png');
+        cardElement.setAttribute('data-id',i);
+        cardElement.addEventListener('click',flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
 
-flipCard(0);
-flipCard(2);
+createBoard();
